22cd87b0d1bd4bc69807836a8d9966ecc83fe245
Robin Sigrist <sigrist@simpit.ch>
2022-06-14 10:25:55 +0200

    added login page and register

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -10,7 +10,15 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'register',
+    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+  },
+
 ];
 
 @NgModule({
