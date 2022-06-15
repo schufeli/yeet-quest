a88564a7722ed41122e4ae7e93cdbcbe9a48e699
Sigrist Robin <42818807+ThankorRS@users.noreply.github.com>
2022-06-15 13:28:17 +0530

    added alert controler for 'add Chat', changed so you can add a chatName

--- app/src/app/home/home.page.ts
+++ app/src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ChatHubService } from '../core/services/chat-hub.service';
 import { SessionService } from '../core/services/session.service';
+import { AlertController } from '@ionic/angular';
 import { UserService } from '../core/services/user.service';
 
 @Component({
@@ -12,10 +13,46 @@ export class HomePage {
   constructor(
     private sessionService: SessionService,
     private chatHubService: ChatHubService,
+    public alertController: AlertController,
     ) { }
 
   ngOnInit() {
     this.sessionService.set({ id: "e3db95b1-4574-4259-9a1f-afcc2f8ffb48", name: "JoeMama" }); // TODO: Remove after development (when better session handling is implemented)
     this.chatHubService.connect();
+
+  }
+  async openAddChatModal(){
+    // let defaultDate = new Date();
+    // let defaultDateFormated = this.datepipe.transform(defaultDate, 'yyyy-MM-dd');
+    const user = await this.sessionService.get();
+    const alert = await this.alertController.create({
+      header: 'Chatname eingeben',
+      inputs: [
+        {
+          name: 'chatName',
+          type: 'text',
+          placeholder: 'Name des Chats eingeben'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+        }, 
+        {
+          text: 'Ok',
+          cssClass: 'primary',
+          handler: (data) => {
+            if (data.chatName !== "") {
+              console.log('this');
+            } else {
+                return false;
+            }
+          }
+        }
+      ]
+    });
+    await alert.present();
   }
 }
