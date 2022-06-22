787d72293c3997ec77e5ded0909414e6d01b1480
Simon Schaufelberger <dextter767@gmail.com>
2022-06-22 10:59:07 +0200

    All done

--- app/src/app/login/login.page.ts
+++ app/src/app/login/login.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { UserService } from '../core/services/user.service'
+import { SessionService } from '../core/services/session.service'
+
 
 @Component({
   selector: 'app-login',
@@ -11,19 +14,27 @@ export class LoginPage implements OnInit {
 
   constructor(
     private router: Router,
+    private userService: UserService,
+    private sessionServcie: SessionService
   ) { }
 
   loginForm : FormGroup;
 
   ngOnInit() {
     this.loginForm = new FormGroup({
-      name: new FormControl('', Validators.required),
+      username: new FormControl('', Validators.required),
     });
   }
 
   sendLoginForm(){
-    console.log('implement me');
-    
+    const user = {
+      name: this.loginForm.get('username').value
+    }
+    this.userService.login(user)
+    .subscribe(result => {
+      this.sessionServcie.set(result['data']);
+      this.router.navigate(['../home'])   
+    })
   }
 
 }
